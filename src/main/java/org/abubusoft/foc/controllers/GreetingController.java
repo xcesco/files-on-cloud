7e9852d1273d83acb3ae3d8adcbb70a5013a0ec1
xcesco <abubusoft@gmail.com>
2019-04-13 01:41:57 +0200

    @wip

--- src/main/java/org/abubusoft/foc/controllers/GreetingController.java
+++ src/main/java/org/abubusoft/foc/controllers/GreetingController.java
@@ -4,20 +4,25 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.abubusoft.foc.controllers.model.Greeting;
 import org.abubusoft.foc.web.V1APIController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @V1APIController
-
 @RestController
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	@GetMapping("/greeting")
+	public ResponseEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return ResponseEntity.ok(new Greeting(counter.incrementAndGet(), String.format(template, name)));
+	}
+	
+	@GetMapping("/greeting2")
+	public ResponseEntity<Greeting> greeting2(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return  ResponseEntity.ok(new Greeting(counter.incrementAndGet(), String.format(template, name)));
 	}
 }
