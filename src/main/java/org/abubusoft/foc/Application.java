c970431eda7e51cad55c8e25080015e165d7e4d0
xcesco <abubusoft@gmail.com>
2019-05-09 03:33:48 +0200

    5: Prima implementazione REST, SERVICE, REPOSITORY
    Task-Url: http://github.com/xcesco/files-on-cloud/issues/5

--- src/main/java/org/abubusoft/foc/Application.java
+++ src/main/java/org/abubusoft/foc/Application.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-
+ 
 @ComponentScan("org.abubusoft.foc")
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application {
