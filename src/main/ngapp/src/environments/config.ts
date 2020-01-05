65d640cd2f7969d9f90a9b2b49029779660b7f75
xcesco <xcesco@gmail.com>
2020-01-05 20:02:18 +0100

    @wip

---
+++ src/main/ngapp/src/environments/config.ts
@@ -0,0 +1,9 @@
+export const firebaseKey = {
+  apiKey: 'AIzaSyDHRei9lTBnZDkK2HA9XDybwO0UKAEsLqY',
+  authDomain: 'programmazione-web-238419.firebaseapp.com',
+  databaseURL: 'https://programmazione-web-238419.firebaseio.com',
+  projectId: 'programmazione-web-238419',
+  storageBucket: 'programmazione-web-238419.appspot.com',
+  messagingSenderId: '249732682505',
+  appId: '1:249732682505:web:7484c190301380ba'
+};
